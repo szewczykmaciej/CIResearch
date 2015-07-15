5b8734364108ee0a25d92eb5ff6232b91e36b6e2
Maciej Szewczyk <maciek@Maciejs-MacBook-Pro-2.local>
2015-07-15 10:47:55 +0200

    Add Xcode project

---
+++ CIResearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CIResearch
+//
+//  Created by Maciej Szewczyk on 15/07/15.
+//  Copyright (c) 2015 Cohesiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CIResearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CIResearch
+//
+//  Created by Maciej Szewczyk on 15/07/15.
+//  Copyright (c) 2015 Cohesiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
