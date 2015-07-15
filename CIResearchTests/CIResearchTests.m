5b8abdb7ded10fae459754f2735d033971d2ae1a
Maciej Szewczyk <maciek@Maciejs-MacBook-Pro-2.local>
2015-07-15 12:00:25 +0200

    Add fail test

--- CIResearchTests/CIResearchTests.m
+++ CIResearchTests/CIResearchTests.m
@@ -30,6 +30,11 @@
     XCTAssert(YES, @"Pass");
 }
 
+- (void)testFailExample {
+    // This is an example of a functional test case.
+    XCTAssert(NO, @"Fail");
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
