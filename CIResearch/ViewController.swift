730b67abca60c67db53e2c0f41090f7b9ecb3a1c
Maciej Szewczyk <maciek@Maciejs-MacBook-Pro.local>
2016-04-05 15:26:12 +0200

    Initial project

---
+++ CIResearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIResearch
+//
+//  Created by Maciej Szewczyk on 05/04/16.
+//  Copyright © 2016 Programista Maciej Szewczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
